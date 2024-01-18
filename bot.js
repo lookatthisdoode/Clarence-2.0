78a8c9cb42cc378c34cb6f5f431adcfa285e3375
andrei <andruhahoke@gmail.com>
2024-01-18 18:08:41 +0100

    changed event handler

--- bot.js
+++ bot.js
@@ -87,6 +87,10 @@ process.on("error", (error) => {
   console.log(error)
 })
 
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error)
+})
+
 player.events.on("playerStart", (queue, track) => {
   // we will later define queue.metadata object while creating the queue
   queue.metadata.channel.send(`Started playing **${track.title}**!`)
